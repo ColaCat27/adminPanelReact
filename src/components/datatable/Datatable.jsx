fbb91528e9d7927fe7f99ba3752129268e1647d8
Vlad <colacat1927@gmail.com>
2022-05-17 15:15:27 +0300

    fetch

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,16 +1,58 @@
 import { React, useState } from 'react';
 import './datatable.scss';
 import { DataGrid } from '@mui/x-data-grid';
-import { userColumns, userRows } from '../../datatablesource';
 import { Link } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 
 const Datatable = () => {
-    const [data, setData] = useState(userRows);
+    const { data, loading, error } = useFetch('/users/');
 
-    const handleDelete = (id) => {
-        setData(data.filter((item) => item.id !== id));
-    };
+    let users = [];
 
+    for (const key in data.data) {
+        users.push(data.data[key]);
+    }
+
+    const userColumns = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            width: 180,
+        },
+        {
+            field: 'user',
+            headerName: 'User',
+            width: 230,
+            renderCell: (params) => {
+                return <div className="cellWithImg">{params.row.username}</div>;
+            },
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            width: 150,
+        },
+        {
+            field: 'age',
+            headerName: 'Age',
+            width: 100,
+        },
+        {
+            field: 'status',
+            headerName: 'Status',
+            width: 160,
+            renderCell: (params) => {
+                return (
+                    <div className={`cellWithStatus ${params.row.status}`}>
+                        {params.row.status}
+                    </div>
+                );
+            },
+        },
+    ];
+    // const handleDelete = (id) => {
+    //     setData(data.filter((item) => item.id !== id));
+    // };
     const actionColumn = [
         {
             field: 'action',
@@ -25,12 +67,7 @@ const Datatable = () => {
                         >
                             <div className="viewButton">View</div>
                         </Link>
-                        <div
-                            className="deleteButton"
-                            onClick={() => handleDelete(params.row.id)}
-                        >
-                            Delete
-                        </div>
+                        <div className="deleteButton">Delete</div>
                     </div>
                 );
             },
@@ -50,11 +87,10 @@ const Datatable = () => {
             </div>
             <DataGrid
                 className="datagrid"
-                rows={data}
+                rows={users}
                 columns={userColumns.concat(actionColumn)}
                 pageSize={9}
                 rowsPerPageOptions={[9]}
-                checkboxSelection
             />
         </div>
     );
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,14 +3,8 @@ import './list.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import Datatable from '../../components/datatable/Datatable';
-import axios from 'axios';
 
 const List = () => {
-    const getData = async () => {
-        const response = await axios.get('/users');
-        console.log(response);
-    };
-
     return (
         <div className="list">
             <Sidebar />
